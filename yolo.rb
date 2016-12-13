51d49116332a4cbde979c639d72c9a98b2e1cb1a
Dimitur Neshev <d.neshev99@gmail.com>
2016-12-13 21:02:21 +0200

    Update yolo.rb

--- yolo.rb
+++ yolo.rb
@@ -3,7 +3,7 @@ require 'csv'
 csv = ARGV[0]
 result=0
 CSV.foreach(csv) do |row|
-if row[0].to_i > 17 && row[1].to_s == "tues"
+if row[0].to_i > 17 && row[1].include?'tues'
 result+=row[2].to_i
 end
 end
